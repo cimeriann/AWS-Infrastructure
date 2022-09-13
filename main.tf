44ee2c55d1f9e177a72a9219fcb3cd9ebd871678
Adebisi Olayinka <67726867+Olaaa30@users.noreply.github.com>
2022-09-13 23:16:13 +0100

    Add whitelist for security group
    I listed my ip address as the only address allowed to access my instance through the security group

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "allow_web" {
         from_port = 443
         to_port = 443
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["102.134.115.161/32"]
     }
 
     ingress {
